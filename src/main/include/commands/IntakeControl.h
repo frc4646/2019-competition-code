954d37b45fabfb7b4f062048880b15a3fb8be443
Alex Freed <ajfreed00@gmail.com>
2019-02-02 11:03:45 -0600

    Actually added files instead of just saying that I did

---
+++ src/main/include/commands/IntakeControl.h
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#ifndef IntakeControl_H
+#define IntakeControl_H
+
+#include <CommandBase.h>
+#include <frc/WPILib.h>
+
+using namespace frc;
+using namespace wpi;
+
+class IntakeControl : public CommandBase {
+ public:
+  IntakeControl();
+  void Initialize() override;
+  void Execute() override;
+  bool IsFinished() override;
+  void End() override;
+  void Interrupted() override;
+};
+#endif
